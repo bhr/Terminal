9360f4ac47b81331d6c58f26b363a2d023019c51
Benedikt Hirmer <benedikt@hirmer.me>
2020-07-23 16:24:16 +0400

    Add prepare_command script to podspec to build hterm if needed

--- build_hterm.sh
+++ build_hterm.sh
@@ -5,6 +5,12 @@ set -Ceu
 HTERM_VERSION='1.89'
 
 CURRENT_DIR=$(cd "$(dirname "$0")" || exit 1; pwd)
+
+if [ -s "${CURRENT_DIR}/Terminal/Resources/hterm_all.js" ]; 
+	then echo "hterm_all.js exists. skip building else"
+	exit 0
+fi
+
 TMPDIR=$(mktemp -d)
 trap 'rm -rf "${TMPDIR}"' EXIT
 
